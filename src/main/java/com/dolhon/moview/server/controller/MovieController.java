7c01ab10994ff9fc2db7af9bb2b273f840f060c6
Dolhon <Dolhon@users.noreply.github.com>
2020-05-23 01:55:03 +0300

    Moved business logic to MovieService class

--- src/main/java/com/dolhon/moview/server/controller/MovieController.java
+++ src/main/java/com/dolhon/moview/server/controller/MovieController.java
@@ -8,35 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dolhon.moview.server.entity.MovieEntity;
 import com.dolhon.moview.server.entity.UserWatchedMovieEntity;
-import com.dolhon.moview.server.repository.MovieRepository;
 import com.dolhon.moview.server.repository.UserWatchedMovieRepository;
 import com.dolhon.moview.server.service.MovieService;
 
 @RestController
 public class MovieController {
-
-	@Autowired 
-	MovieRepository repository;
-
-	@Autowired 
-	UserWatchedMovieRepository userWatchedMovieRepository;
 	
 	@Autowired
 	MovieService service;
 
 	@RequestMapping(value = "/api/movies", method = RequestMethod.GET)
 	public Iterable<MovieEntity> fetchMovies() {
-		return repository.findAll();
+		return service.fetchMovies();
 	}
 	
 	@RequestMapping(value = "/api/movies/watched", method = RequestMethod.GET)
-	public Iterable<UserWatchedMovieEntity> fetchUsersWatchedMovies() {
-		return userWatchedMovieRepository.findAllByUserId(1L);
+	public Iterable<UserWatchedMovieEntity> fetchUserWatchedMovies() {
+		return service.fetchUserWatchedMovies(1L);
 	}
 	
 	@RequestMapping(value = "/api/movies/{id}/watched", method = RequestMethod.POST)
 	public void saveWatchedMovie(@PathVariable Long id) {
-		
 		service.saveWatchedMovie(id);
 	}
 }
--- src/main/java/com/dolhon/moview/server/service/MovieService.java
+++ src/main/java/com/dolhon/moview/server/service/MovieService.java
@@ -6,27 +6,38 @@ import org.springframework.stereotype.Service;
 import com.dolhon.moview.server.entity.MovieEntity;
 import com.dolhon.moview.server.entity.UserEntity;
 import com.dolhon.moview.server.entity.UserWatchedMovieEntity;
+import com.dolhon.moview.server.repository.MovieRepository;
 import com.dolhon.moview.server.repository.UserWatchedMovieRepository;
 
 @Service
 public class MovieService {
-	
-	@Autowired 
+
+	@Autowired
+	MovieRepository repository;
+
+	@Autowired
 	UserWatchedMovieRepository userWatchedMovieRepository;
-	
+
 	public void saveWatchedMovie(Long id) {
-		
+
 		UserWatchedMovieEntity entity = new UserWatchedMovieEntity();
 		MovieEntity movie = new MovieEntity();
 		UserEntity user = new UserEntity();
-		
+
 		movie.setId(id);
 		user.setId(1L);
-		
+
 		entity.setMovie(movie);
 		entity.setUser(user);
-		
+
 		userWatchedMovieRepository.save(entity);
 	}
-	
+
+	public Iterable<MovieEntity> fetchMovies() {
+		return repository.findAll();
+	}
+
+	public Iterable<UserWatchedMovieEntity> fetchUserWatchedMovies(long usedId) {
+		return userWatchedMovieRepository.findAllByUserId(usedId);
+	}
 }
